17b37b40984dae311df2910806284e9c12031cdc
Ting Yang <yt0701@gmail.com>
2024-03-04 16:46:34 -0800

    Add solution for twosum.

---
+++ src/TwoSum/Solution.java
@@ -0,0 +1,21 @@
+package TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valueIndex = new HashMap<>();
+        int[] result = new int[2];
+        for (int index = 0; index < nums.length; index++) {
+            int remains = target - nums[index];
+            if (valueIndex.containsKey(remains)) {
+                result[0] = valueIndex.get(remains);
+                result[1] = index;
+                break;
+            }
+            valueIndex.put(nums[index], index);
+        }
+        return result;
+    }
+}
