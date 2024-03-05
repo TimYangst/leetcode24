22efc6adfaf0e0537136d7a1183b37c433d663e7
Ting Yang <yt0701@gmail.com>
2024-03-04 17:53:12 -0800

    Add solution for 3.Longest Substring Without Repeating Characters.

---
+++ src/P3LongestSubstringWRC/Solution.java
@@ -0,0 +1,23 @@
+package P3LongestSubstringWRC;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> charIndex = new HashMap<>();
+        int result = 0;
+        int lastCut = -1;
+        for (int index = 0; index < s.length(); index++) {
+            int previous = -1;
+            if (charIndex.containsKey(s.charAt(index))) {
+                previous = charIndex.get(s.charAt(index));
+            }
+            if (previous > lastCut) {
+                lastCut = previous;
+            }
+            if (index - lastCut > result) {
+                result = index - lastCut;
+            }
+            charIndex.put(s.charAt(index), index);
+        }
+        return result;
+    }
+}
