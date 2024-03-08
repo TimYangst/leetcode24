6e8f7716cc7176d15af98fcc542df8e019f379ac
Ting Yang <yt0701@gmail.com>
2024-03-07 22:17:18 -0800

    Add solution for P32

---
+++ src/P31LongestValidParentheses/Solution.java
@@ -0,0 +1,35 @@
+package P31LongestValidParentheses;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s.length() <= 1)
+            return 0;
+        int[] f = new int[s.length()];
+        int result = 0;
+        f[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                f[i] = 0;
+            } else {
+                int j = i - 1;
+                int k = 1;
+                while (k >= 1 && j >= 0) {
+                    if (s.charAt(j) == '(')
+                        k--;
+                    else
+                        k++;
+                    j--;
+                }
+                if (k == 0) {
+                    if (j < 0) {
+                        f[i] = i - j;
+                    } else
+                        f[i] = i - j + f[j];
+                }
+            }
+            if (f[i] > result)
+                result = f[i];
+        }
+        return result;
+    }
+}
