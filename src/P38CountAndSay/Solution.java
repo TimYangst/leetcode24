55fa70b12f13bbecbe6eb4e9284c76eb571a0d23
Ting Yang <yt0701@gmail.com>
2024-03-11 17:26:40 -0700

    Add solution for p38

---
+++ src/P38CountAndSay/Solution.java
@@ -0,0 +1,25 @@
+package P38CountAndSay;
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1)
+            return "1";
+        String current = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int j = 0;
+            while (j < current.length()) {
+                int count = 1;
+                char c = current.charAt(j);
+                while (j + 1 < current.length() && current.charAt(j + 1) == current.charAt(j)) {
+                    j++;
+                    count++;
+                }
+                sb.append(count).append(current.charAt(j));
+                j++;
+            }
+            current = sb.toString();
+        }
+        return current;
+    }
+}
