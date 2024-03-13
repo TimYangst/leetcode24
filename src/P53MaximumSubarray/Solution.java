9f885d84cb2466643baf486ddcf10b6f75f3e6fe
Ting Yang <yt0701@gmail.com>
2024-03-13 11:25:11 -0700

    p53

---
+++ src/P53MaximumSubarray/Solution.java
@@ -0,0 +1,22 @@
+package P53;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int i = 0;
+        if (nums.length == 0)
+            return 0;
+        int current = 0;
+        while (i < nums.length) {
+            if (current + nums[i] > result)
+                result = current + nums[i];
+
+            if (current + nums[i] > 0) {
+                current += nums[i];
+            } else
+                current = 0;
+            i++;
+        }
+        return result;
+    }
+}
