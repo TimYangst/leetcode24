f789e055323381d50db801998a3ede8b3bd5325d
Ting Yang <yt0701@gmail.com>
2024-03-16 18:43:32 -0700

    t150p80

---
+++ src/Top150/P80RemoveDuplicatesFromSortedArrayII/Solution.java
@@ -0,0 +1,17 @@
+package Top150.P80RemoveDuplicatesFromSortedArrayII;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        int j = 0;
+        while (j < nums.length) {
+            nums[i++] = nums[j++];
+            if (j < nums.length && nums[j] == nums[j - 1]) {
+                nums[i++] = nums[j++];
+            }
+            while (j < nums.length && nums[j] == nums[j - 1])
+                j++;
+        }
+        return i;
+    }
+}
