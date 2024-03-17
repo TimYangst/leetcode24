fa3f46f85adb2d196d0211622823f9ffae2e4885
Ting Yang <yt0701@gmail.com>
2024-03-17 16:45:53 -0700

    Add solution for t150 p122

---
+++ src/Top150/P122BestTimetoBuyandSellStockII/Solution1.java
@@ -0,0 +1,25 @@
+package Top150.P122BestTimetoBuyandSellStockII;
+
+class Solution1 {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1)
+            return 0;
+        int result = 0;
+        int currentLow = prices[0];
+        int currentHigh = prices[0];
+        int currentProfit = 0;
+        for (int j = 1; j < prices.length; j++) {
+            if (prices[j] > currentHigh) {
+                currentHigh = prices[j];
+                currentProfit = currentHigh - currentLow;
+            } else {
+                result += currentProfit;
+                currentProfit = 0;
+                currentLow = prices[j];
+                currentHigh = prices[j];
+            }
+        }
+        result += currentProfit;
+        return result;
+    }
+}
