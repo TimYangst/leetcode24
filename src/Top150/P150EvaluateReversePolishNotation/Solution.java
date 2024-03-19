aae421fa0dca8945ef6a80e770123370284e9752
Ting Yang <yt0701@gmail.com>
2024-03-19 15:24:20 -0700

    t150p150

---
+++ src/Top150/P150EvaluateReversePolishNotation/Solution.java
@@ -0,0 +1,27 @@
+package Top150.P150EvaluateReversePolishNotation;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        Set<String> operates = Set.of("+", "-", "*", "/");
+        int e1, e2;
+        for (int i = 0; i < tokens.length; i++) {
+            if (operates.contains(tokens[i])) {
+                e2 = stack.pop();
+                e1 = stack.pop();
+                if ("+".equals(tokens[i])) {
+                    stack.push(e1 + e2);
+                } else if ("-".equals(tokens[i])) {
+                    stack.push(e1 - e2);
+                } else if ("*".equals(tokens[i])) {
+                    stack.push(e1 * e2);
+                } else {
+                    stack.push((int) (e1 / e2));
+                }
+            } else {
+                stack.push(Integer.valueOf(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
