4fe78aef5eeec9293899167f617c0977c27b4b4f
Ting Yang <yt0701@gmail.com>
2024-03-18 17:09:05 -0700

    Add optimized solution for t150p128

--- src/Top150/P128LongestConsecutiveSequence/Solution.java
+++ src/Top150/P128LongestConsecutiveSequence/Solution.java
@@ -1,64 +1,25 @@
 package Top150.P128LongestConsecutiveSequence;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length <= 1)
             return nums.length;
-        Map<Integer, Integer> indexMap = new HashMap<>();
-        int[] parent = new int[nums.length];
-        int[] count = new int[nums.length];
+        Set<Integer> numbers = new HashSet<>();
+        int result = 1;
         for (int i = 0; i < nums.length; i++) {
-            count[i] = 1;
-            parent[i] = i;
+            numbers.add(nums[i]);
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (!indexMap.containsKey(nums[i])) {
-                indexMap.put(nums[i], i);
-                insert(nums[i], indexMap, parent, count);
+        for (int i : numbers) {
+            if (!numbers.contains(i - 1)) {
+                int j = i + 1;
+                while (numbers.contains(j))
+                    j++;
+                result = Math.max(result, j - i);
             }
         }
-        int result = 1;
-        for (int i = 0; i < nums.length; i++)
-            result = Math.max(result, count[i]);
         return result;
-    }
-
-    void insert(int n, Map<Integer, Integer> indexMap, int[] parent, int[] count) {
-        int current = indexMap.get(n);
-        int index;
-        if (indexMap.containsKey(n - 1)) {
-            index = indexMap.get(n - 1);
-            merge(current, index, parent, count);
-        }
-        if (indexMap.containsKey(n + 1)) {
-            index = indexMap.get(n + 1);
-            merge(current, index, parent, count);
-        }
-    }
-
-    int getParent(int index, int[] parent) {
-        if (parent[index] != index) {
-            parent[index] = getParent(parent[index], parent);
-        }
-        return parent[index];
-    }
-
-    void merge(int i1, int i2, int[] parent, int[] count) {
-        int p1 = getParent(i1, parent);
-        int p2 = getParent(i2, parent);
-        if (p1 == p2)
-            return;
-        if (count[p1] >= count[p2]) {
-            parent[p2] = p1;
-            parent[i2] = p1;
-            count[p1] += count[p2];
-        } else {
-            parent[p1] = p2;
-            parent[i1] = p2;
-            count[p2] += count[p1];
-        }
     }
 }
