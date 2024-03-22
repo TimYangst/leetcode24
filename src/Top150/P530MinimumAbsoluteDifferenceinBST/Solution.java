e924a90cf5b58a168da976914828d6843baca5c6
Ting Yang <yt0701@gmail.com>
2024-03-21 22:42:23 -0700

    t150p530

---
+++ src/Top150/P530MinimumAbsoluteDifferenceinBST/Solution.java
@@ -0,0 +1,49 @@
+package Top150.P530MinimumAbsoluteDifferenceinBST;
+
+/**
+ * Definition for a binary tree node.
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null)
+            return 0;
+        int result = Integer.MAX_VALUE;
+        TreeNode pre = null;
+        Stack<TreeNode> midNodes = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !midNodes.empty()) {
+            if (current == null) {
+                current = midNodes.pop();
+                if (pre != null) {
+                    result = Math.min(result, current.val - pre.val);
+                }
+                pre = current;
+                current = current.right;
+            } else {
+                midNodes.push(current);
+                current = current.left;
+            }
+        }
+        return result;
+    }
+}
