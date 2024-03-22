159c012822269ac67cd8023c93c7013cec9346cb
Ting Yang <yt0701@gmail.com>
2024-03-21 19:09:22 -0700

    t150p102

---
+++ src/Top150/P102BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,50 @@
+package Top150.P102BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        visit(root, 0, result);
+        return result;
+    }
+
+    void visit(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null)
+            return;
+        if (result.size() <= level) {
+            List<Integer> newList = new ArrayList<Integer>();
+            newList.add(root.val);
+            result.add(newList);
+        } else {
+            result.get(level).add(root.val);
+        }
+        visit(root.left, level + 1, result);
+        visit(root.right, level + 1, result);
+    }
+}
