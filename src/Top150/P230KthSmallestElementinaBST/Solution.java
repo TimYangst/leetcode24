3738c030cd0de02f9d02b4fa3f11adeb5d8b139d
Ting Yang <yt0701@gmail.com>
2024-03-21 22:50:01 -0700

    t150p230

---
+++ src/Top150/P230KthSmallestElementinaBST/Solution.java
@@ -0,0 +1,46 @@
+package Top150.P230KthSmallestElementinaBST;
+
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (k > 0) {
+            if (current == null) {
+                current = stack.pop();
+                k--;
+                if (k == 0)
+                    return current.val;
+                current = current.right;
+            } else {
+                stack.push(current);
+                current = current.left;
+            }
+        }
+        return 0;
+    }
+}
