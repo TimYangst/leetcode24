05d8d23e4aa07bbe1a5d3acba587887c81f51444
Ting Yang <yt0701@gmail.com>
2024-03-22 20:42:03 -0700

    t150p91

---
+++ src/Top150/P918MaximumSumCircularSubarray/Solution1.java
@@ -0,0 +1,45 @@
+package Top150.P918MaximumSumCircularSubarray;
+
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int result = nums[0];
+        int i = 0;
+        while (i < nums.length && nums[i] < 0) {
+            result = Math.max(result, nums[i]);
+            i++;
+        }
+        if (i == nums.length)
+            return result;
+        result = nums[i];
+        int start = i;
+        int remains = 0;
+        while (i < nums.length) {
+            if (nums[i] < 0) {
+                i++;
+                remains = 0;
+                start = i;
+            } else {
+                remains = nums[i];
+                result = Math.max(result, remains);
+                int j = i + 1;
+                while (j < start + nums.length && remains + getValue(nums, j) >= 0) {
+                    remains += getValue(nums, j);
+                    result = Math.max(result, remains);
+                    j++;
+                }
+                i++;
+                remains = 0;
+                start = i;
+            }
+        }
+        return result;
+    }
+
+    int getValue(int[] nums, int j) {
+        if (j >= nums.length)
+            return nums[j - nums.length];
+        return nums[j];
+    }
+}
