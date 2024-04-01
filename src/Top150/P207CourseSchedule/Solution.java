2198cda941f174749c309c6e4a4780fe3e2180a2
Ting Yang <yt0701@gmail.com>
2024-04-01 15:48:49 -0700

    t150p207

---
+++ src/Top150/P207CourseSchedule/Solution.java
@@ -0,0 +1,51 @@
+package Top150.P207CourseSchedule;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 1) return true;
+        Map<Integer, Set<Integer>> coursePres = new HashMap<>();
+        for (int i = 0; i< prerequisites.length; i++) {
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            if (coursePres.containsKey(a)) {
+                coursePres.get(a).add(b);
+            } else {
+                Set<Integer> preS = new HashSet<>();
+                preS.add(b);
+                coursePres.put(a, preS);
+            }
+        }
+        int taken = 0;
+        Queue<Integer> courses = new LinkedList<>();
+        for (int i = 0 ; i < numCourses; i++) {
+            if (!coursePres.containsKey(i)) {
+                taken++;
+                courses.add(i);
+            }
+        }
+        while (taken < numCourses && !courses.isEmpty()) {
+            int current = courses.poll();
+            for (int i = 0; i< numCourses; i++) {
+                if (coursePres.containsKey(i)) {
+                    Set<Integer> preS = coursePres.get(i);
+                    if (preS.contains(current)) {
+                        preS.remove(current);
+                        if (preS.isEmpty()) {
+                            taken ++;
+                            courses.add(i);
+                            if (taken == numCourses) break;
+                        }
+                    }
+                }
+            }
+        }
+        return taken == numCourses;
+    }
+}
