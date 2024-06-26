63aeed1702c1e459cc31bc44150e599861d82949
TimYangst <yt0701@gmail.com>
2024-06-26 15:31:43 -0700

    p108

---
+++ src/Top150/P108ConvertSortedArrayToBST/Solution.java
@@ -0,0 +1,32 @@
+package Top150.P108ConvertSortedArrayToBST;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    TreeNode buildBST(int[] nums, int b, int length) {
+        if (length <= 0) return null;
+        int mid = b + length / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums, b, mid - b);
+        root.right = buildBST(nums, mid + 1, length - (mid - b) - 1);
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return buildBST(nums, 0, nums.length);
+    }
+}
