1c37b70cdcf6b6cedbfe7761319f493bd98910ef
TimYangst <yt0701@gmail.com>
2024-08-10 14:19:52 -0700

    lazyarray

--- src/Interviews/LazyArray/LazyArray.java
+++ src/Interviews/LazyArray/LazyArray.java
@@ -1,12 +1,57 @@
 package Interviews.LazyArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class LazyArray {
 
-    private List<Integer> elements; 
-    public LazyArray(int[] element) {
+    private List<Integer> elements;
+    private List<Function<List<Integer>, List<Integer>>> operations;
 
+    public LazyArray(int[] array) {
+
+        this.elements = new ArrayList<>(Arrays.stream(array).boxed().collect(Collectors.toList()));
+        this.operations = new ArrayList<>();
     }
-    
-    
+
+    public List<Integer> toList() {
+        return evaluate();
+    }
+
+    public LazyArray map(Function<Integer, Integer> mapOp) {
+        operations.add(list -> list.stream().map(mapOp).collect(Collectors.toList()));
+        return this;
+    }
+
+    public LazyArray filter(Predicate<Integer> predOp) {
+        operations.add(list -> list.stream().filter(predOp).collect(Collectors.toList()));
+        return this;
+    }
+
+    public List<Integer> evaluate() {
+        List<Integer> result = elements;
+        for (Function<List<Integer>, List<Integer>> op : operations) {
+            result = op.apply(result);
+        }
+        return result;
+    }
+
+    public int indexOf(Integer value) {
+        List<Integer> evaluated = evaluate();
+        return evaluated.indexOf(value);
+    }
+
+    public static void main(String[] args) {
+        LazyArray array = new LazyArray(new int[] { 1, 2, 3 });
+        System.out.println(array.map(a -> a * 3).indexOf(3));
+
+        for (int i : array.toList()) {
+            System.out.println(i);
+        }
+    }
+
 }
