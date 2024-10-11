c4a25ab7fccd457bd132c9e631fc134b6b537466
TimYangst <yt0701@gmail.com>
2024-10-11 13:47:50 -0700

    optimization

--- src/P188BestTimetoBuyandSellStockIV/Solution.java
+++ src/P188BestTimetoBuyandSellStockIV/Solution.java
@@ -1,28 +1,37 @@
 package P188BestTimetoBuyandSellStockIV;
 
 class Solution {
+
+    private int trySolve(int index, int canBuyAStock, int[][][] dp, int[] prices, int capacity) {
+        if (capacity == 0 || index == prices.length)
+            return 0;
+        if (dp[index][canBuyAStock][capacity] != -1)
+            return dp[index][canBuyAStock][capacity];
+
+        if (canBuyAStock == 1) {
+            dp[index][canBuyAStock][capacity] = Math.max(
+                    -prices[index] + trySolve(index + 1, 0, dp, prices, capacity),
+                    trySolve(index + 1, 1, dp, prices, capacity));
+        } else {
+            dp[index][canBuyAStock][capacity] = Math.max(
+                    prices[index] + trySolve(index + 1, 1, dp, prices, capacity - 1),
+                    trySolve(index + 1, 0, dp, prices, capacity));
+        }
+        return dp[index][canBuyAStock][capacity];
+    }
+
     public int maxProfit(int k, int[] prices) {
         if (prices.length <= 1 || k == 0)
             return 0;
         int n = prices.length;
-        int[][] f = new int[k + 1][n + 1];
-        int minV = prices[0];
-        for (int i = 1; i < n; i++) {
-            if (minV > prices[i]) {
-                minV = prices[i];
-            }
-            f[0][i] = Math.max(f[0][i - 1], prices[i] - minV);
-        }
-
-        for (int j = 1; j < k; j++) {
-            f[j][1] = f[j - 1][1];
-            for (int i = 2; i < n; i++) {
-                f[j][i] = Math.max(f[j - 1][i], f[j][i - 1]);
-                for (int l = 1; l < i; l++) {
-                    f[j][i] = Math.max(f[j][i], (prices[i] - prices[l]) + f[j - 1][l - 1]);
+        int[][][] dp = new int[n + 1][2][k + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= 1; j++) {
+                for (int l = 0; l <= k; l++) {
+                    dp[i][j][l] = -1;
                 }
             }
         }
-        return f[k - 1][n - 1];
+        return trySolve(0, 1, dp, prices, k);
     }
 }
