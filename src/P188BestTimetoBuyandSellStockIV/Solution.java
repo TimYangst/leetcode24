cc88e2cec2eb845c68f46a6998fa152c81bacb7a
TimYangst <yt0701@gmail.com>
2024-10-11 13:09:29 -0700

    p188

---
+++ src/P188BestTimetoBuyandSellStockIV/Solution.java
@@ -0,0 +1,28 @@
+package P188BestTimetoBuyandSellStockIV;
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length <= 1 || k == 0)
+            return 0;
+        int n = prices.length;
+        int[][] f = new int[k + 1][n + 1];
+        int minV = prices[0];
+        for (int i = 1; i < n; i++) {
+            if (minV > prices[i]) {
+                minV = prices[i];
+            }
+            f[0][i] = Math.max(f[0][i - 1], prices[i] - minV);
+        }
+
+        for (int j = 1; j < k; j++) {
+            f[j][1] = f[j - 1][1];
+            for (int i = 2; i < n; i++) {
+                f[j][i] = Math.max(f[j - 1][i], f[j][i - 1]);
+                for (int l = 1; l < i; l++) {
+                    f[j][i] = Math.max(f[j][i], (prices[i] - prices[l]) + f[j - 1][l - 1]);
+                }
+            }
+        }
+        return f[k - 1][n - 1];
+    }
+}
