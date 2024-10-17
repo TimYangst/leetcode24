ed9bacd32fa8b7b221db4ec91694179e81c5e7ae
TimYangst <yt0701@gmail.com>
2024-10-17 14:41:25 -0700

    opt

--- src/P149MaxPointsOnALine/Solution.java
+++ src/P149MaxPointsOnALine/Solution.java
@@ -1,12 +1,23 @@
 package P149MaxPointsOnALine;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    boolean onSameLine(int[] p0, int[] p1, int[] p2) {
-        return (p1[0] - p0[0]) * (p2[1] - p0[1]) == (p2[0] - p0[0]) * (p1[1] - p0[1]);
+    private int gcd(int a, int b) {
+        if (b == 0 || a == 0)
+            return 0;
+        if (b % a == 0)
+            return a;
+        return gcd(b % a, a);
+    }
+
+    private String getSlopeTag(int[] p0, int[] p1) {
+        int gcd = gcd(p1[1] - p0[1], p1[0] - p0[0]);
+        if (gcd == 0)
+            return "0";
+        return String.format("%s/%s", (p1[1] - p0[1]) / gcd, (p1[0] - p0[0]) / gcd);
     }
 
     public int maxPoints(int[][] points) {
@@ -14,21 +25,23 @@ class Solution {
             return points.length;
         Arrays.sort(points, (a, b) -> ((a[0] != b[0]) ? (a[0] - b[0]) : (b[1] - a[1])));
         int result = 2;
-        for (int i = 0; i < points.length - result; i++) {
-            Set<Integer> taken = new HashSet<>();
-            for (int j = i + 1; j < points.length; j++) {
-                if (!taken.contains(j)) {
-                    taken.add(j);
-                    int current = 2;
-                    for (int k = j + 1; k < points.length; k++) {
-                        if (onSameLine(points[i], points[j], points[k])) {
-                            taken.add(k);
-                            current++;
-                        }
-                    }
-                    if (current > result)
-                        result = current;
-                }
+        for (int i = 0; i + result < points.length; i++) {
+            int count = 0;
+            int j = i;
+            while (j < points.length && points[j][0] == points[i][0]) {
+                j++;
+                count++;
+            }
+            if (count > result)
+                result = count;
+            Map<String, Integer> countMap = new HashMap<>();
+            for (int k = j; k < points.length; k++) {
+                countMap.compute(getSlopeTag(points[i], points[k]),
+                        (key, value) -> (value == null ? 2 : value + 1));
+            }
+            for (int value : countMap.values()) {
+                if (value > result)
+                    result = value;
             }
         }
         return result;
