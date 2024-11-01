6535dec3c014073473bfde56a73daa8edc6213d3
TimYangst <yt0701@gmail.com>
2024-10-31 17:02:59 -0700

    updated

--- src/P636/Solution.java
+++ src/P636/Solution.java
@@ -8,30 +8,20 @@ class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         Stack<Integer> stack = new Stack<>();
         int[] runTime = new int[n];
-        int lastStart = 0;
-        int lastEnd = -1;
+        int lastRunPoint = 0;
         for (String log : logs) {
             String[] data = log.split(":");
             int task = Integer.valueOf(data[0]);
             int time = Integer.valueOf(data[2]);
-
             if ("start".equals(data[1])) {
                 if (!stack.isEmpty()) {
-                    if (lastStart > lastEnd) {
-                        runTime[stack.peek()] += time - lastStart;
-                    } else {
-                        runTime[stack.peek()] += time - lastEnd - 1;
-                    }
+                    runTime[stack.peek()] += time - lastRunPoint;
                 }
                 stack.push(task);
-                lastStart = time;
+                lastRunPoint = time;
             } else {
-                if (lastStart > lastEnd) {
-                    runTime[stack.pop()] += time - lastStart + 1;
-                } else {
-                    runTime[stack.pop()] += time - lastEnd;
-                }
-                lastEnd = time;
+                runTime[stack.pop()] += time - lastRunPoint + 1;
+                lastRunPoint = time + 1;
             }
         }
         return runTime;
