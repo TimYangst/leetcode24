fc459dd4493f74b5bbbdc8f70e5de5e55b6f9e97
TimYangst <yt0701@gmail.com>
2024-12-12 18:52:34 -0800

    637

--- src/Top150/P637AverageofLevelsinBinaryTree/Solution.java
+++ src/Top150/P637AverageofLevelsinBinaryTree/Solution.java
@@ -1,54 +1,31 @@
 package Top150.P637AverageofLevelsinBinaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-/**
- * Definition for a binary tree node.
- */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+import java.util.Queue;
 
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
         List<Double> result = new ArrayList<Double>();
         if (root == null)
             return result;
-        List<Integer> count = new ArrayList<>();
-        visit(root, result, count, 0);
-        for (int i = 0; i < count.size(); i++) {
-            result.set(i, (result.get(i) / count.get(i)));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            double sum = 0.0;
+            for (int i = 0; i < n; i++) {
+                TreeNode current = queue.poll();
+                sum += current.val;
+                if (current.left != null)
+                    queue.offer(current.left);
+                if (current.right != null)
+                    queue.offer(current.right);
+            }
+            result.add(sum / n);
         }
         return result;
     }
 
-    void visit(TreeNode root, List<Double> result, List<Integer> count, int level) {
-        if (root == null)
-            return;
-        if (level == count.size()) {
-            result.add((double) root.val);
-            count.add(1);
-        } else {
-            result.set(level, result.get(level) + root.val);
-            count.set(level, count.get(level) + 1);
-        }
-        visit(root.left, result, count, level + 1);
-        visit(root.right, result, count, level + 1);
-    }
 }
