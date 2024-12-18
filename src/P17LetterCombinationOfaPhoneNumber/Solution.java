3b63de1244bf4750c7f4b418e1ef3808a88da2db
TimYangst <yt0701@gmail.com>
2024-12-17 17:27:09 -0800

    17

--- src/P17LetterCombinationOfaPhoneNumber/Solution.java
+++ src/P17LetterCombinationOfaPhoneNumber/Solution.java
@@ -4,44 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if (digits.isEmpty())
-            return result;
-        int[] size = new int[10];
-        for (int i = 0; i < 10; i++) {
-            if (i < 2) {
-                size[i] = 0;
-            } else
-                size[i] = 3;
-            if (i == 7 || i == 9)
-                size[i]++;
-        }
-        char[][] mapping = getCharMapping(size);
-        generate(digits, 0, "", mapping, size, result);
-        return result;
-    }
-
-    void generate(String digits, int index, String current, char[][] mapping, int[] size, List<String> result) {
-        char c = digits.charAt(index);
-        for (int i = 0; i < size[c - '0']; i++) {
-            String newS = current + mapping[c - '0'][i];
-            if (index < digits.length() - 1) {
-                generate(digits, index + 1, newS, mapping, size, result);
-            } else {
-                result.add(newS);
-            }
-        }
-    }
-
-    char[][] getCharMapping(int[] size) {
+    private char[][] getMapping() {
+        char[][] mapping = new char[10][];
         char c = 'a';
-        char[][] mapping = new char[10][4];
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 0; j < size[i]; j++) {
-                mapping[i][j] = c++;
+        for (int i = 0; i < 10; i++) {
+            if (i < 2)
+                mapping[i] = new char[0];
+            else if (i != 7 && i != 9) {
+                mapping[i] = new char[3];
+            } else {
+                mapping[i] = new char[4];
             }
+            for (int j = 0; j < mapping[i].length; j++)
+                mapping[i][j] = c++;
         }
         return mapping;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> current = new ArrayList<>();
+        if (digits.isEmpty())
+            return current;
+        char[][] mapping = getMapping();
+        current.add(digits);
+        for (int i = 0; i < digits.length(); i++) {
+            List<String> next = new ArrayList<>();
+            for (String str : current) {
+                char[] arr = str.toCharArray();
+                int index = arr[i] - '0';
+                for (char c : mapping[index]) {
+                    arr[i] = c;
+                    next.add(new String(arr));
+                }
+            }
+            current = next;
+        }
+        return current;
     }
 }
---
+++ src/P17LetterCombinationOfaPhoneNumber/Solution1.java
@@ -0,0 +1,47 @@
+package P17LetterCombinationOfaPhoneNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution1 {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.isEmpty())
+            return result;
+        int[] size = new int[10];
+        for (int i = 0; i < 10; i++) {
+            if (i < 2) {
+                size[i] = 0;
+            } else
+                size[i] = 3;
+            if (i == 7 || i == 9)
+                size[i]++;
+        }
+        char[][] mapping = getCharMapping(size);
+        generate(digits, 0, "", mapping, size, result);
+        return result;
+    }
+
+    void generate(String digits, int index, String current, char[][] mapping, int[] size, List<String> result) {
+        char c = digits.charAt(index);
+        for (int i = 0; i < size[c - '0']; i++) {
+            String newS = current + mapping[c - '0'][i];
+            if (index < digits.length() - 1) {
+                generate(digits, index + 1, newS, mapping, size, result);
+            } else {
+                result.add(newS);
+            }
+        }
+    }
+
+    char[][] getCharMapping(int[] size) {
+        char c = 'a';
+        char[][] mapping = new char[10][4];
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 0; j < size[i]; j++) {
+                mapping[i][j] = c++;
+            }
+        }
+        return mapping;
+    }
+}
