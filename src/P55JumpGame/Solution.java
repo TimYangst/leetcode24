1a77f3d83dfd8ef04beeed03aa87b71aa47df547
TimYangst <yt0701@gmail.com>
2024-12-18 16:55:13 -0800

    55

--- src/P55JumpGame/Solution.java
+++ src/P55JumpGame/Solution.java
@@ -2,14 +2,11 @@ package P55JumpGame;
 
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] m = new int[nums.length];
-        m[0] = nums[0];
-        if (nums.length == 1)
-            return true;
-        for (int i = 1; i < nums.length; i++) {
-            if (m[i - 1] < i)
+        int currentMax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (currentMax < i)
                 return false;
-            m[i] = Math.max(m[i - 1], nums[i] + i);
+            currentMax = Math.max(currentMax, nums[i] + i);
         }
         return true;
     }
