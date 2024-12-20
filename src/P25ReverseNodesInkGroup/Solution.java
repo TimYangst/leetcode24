b6a2cb44e151d6146b387d76c4c34f3efb8970e5
TimYangst <yt0701@gmail.com>
2024-12-19 16:25:20 -0800

    25

--- src/P25ReverseNodesInkGroup/Solution.java
+++ src/P25ReverseNodesInkGroup/Solution.java
@@ -2,29 +2,26 @@ package P25ReverseNodesInkGroup;
 
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k == 1)
+        if (k <= 1 || head == null)
             return head;
-        ListNode tail = head;
-        ListNode result = null;
-        int i = k;
-        while (tail != null && i > 0) {
-            i--;
-            result = tail;
-            tail = tail.next;
+        int r = k;
+        ListNode next = head;
+        ListNode last = null;
+        while (r > 0 && next != null) {
+            last = next;
+            next = next.next;
+            r--;
         }
-        if (i > 0)
+        if (r > 0)
             return head;
-        ListNode next = head.next;
-        ListNode nextnext = head.next.next;
-        ListNode current = head;
-        do {
-            next.next = current;
-            current = next;
-            next = nextnext;
-            if (nextnext != null)
-                nextnext = nextnext.next;
-        } while (current != result);
-        head.next = reverseKGroup(tail, k);
-        return result;
+        ListNode newHead = head;
+        while (newHead != last) {
+            ListNode tmp = head.next;
+            head.next = head.next.next;
+            tmp.next = newHead;
+            newHead = tmp;
+        }
+        head.next = reverseKGroup(next, k);
+        return newHead;
     }
 }
---
+++ src/P25ReverseNodesInkGroup/Solution2.java
@@ -0,0 +1,30 @@
+package P25ReverseNodesInkGroup;
+
+class Solution2 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1)
+            return head;
+        ListNode tail = head;
+        ListNode result = null;
+        int i = k;
+        while (tail != null && i > 0) {
+            i--;
+            result = tail;
+            tail = tail.next;
+        }
+        if (i > 0)
+            return head;
+        ListNode next = head.next;
+        ListNode nextnext = head.next.next;
+        ListNode current = head;
+        do {
+            next.next = current;
+            current = next;
+            next = nextnext;
+            if (nextnext != null)
+                nextnext = nextnext.next;
+        } while (current != result);
+        head.next = reverseKGroup(tail, k);
+        return result;
+    }
+}
