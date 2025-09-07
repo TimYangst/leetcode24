ec6d8a57213703d8c62559c11be2f299d53ffa42
TimYangst <yt0701@gmail.com>
2025-09-06 19:31:08 -0700

    P2537 opt

--- src/P2537/Solution.java
+++ src/P2537/Solution.java
@@ -5,38 +5,25 @@ import java.util.Map;
 
 class Solution {
     public long countGood(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        int b = 0;
-        int e = 0;
-        int n = nums.length;
-        int current = 0;
-        long result = 0;
-        count.put(nums[0], 1);
-        int lastB = -1;
-        int lastE = -1;
-        while (e < nums.length - 1) {
-            e++; 
-            current += count.getOrDefault(nums[e], 0);
-            System.out.println(current + " "+ b + " "+ e);
-            count.compute(nums[e], (key, v) -> v==null? 1: v +1);
-            if (current >= k) {
-                while (b < e && current - (count.get(nums[b]) - 1) >= k){
-                    current -= (count.get(nums[b]) - 1);
-                    count.compute(nums[b], (key, v) -> v-1);
-                    b++;
-                } 
-                if (lastB == -1) {
-                    result += (b + 1) * (n-e);
-                } else {
-                    result += (b - lastB) * (n - e );
-                }
-                lastB = b;
-                lastE = e; 
-                current -= (count.get(nums[b]) - 1);
-                count.compute(nums[b], (key, v) -> v - 1);
-                b++;
+        Map<Integer, Integer> freq = new HashMap<>();
+        int left = 0;
+        long pairCount = 0;
+        long goodSubarrays = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            int count = freq.getOrDefault(nums[right], 0);
+            pairCount += count;
+            freq.put(nums[right], count + 1);
+
+            while (pairCount >= k) {
+                goodSubarrays += nums.length - right;
+                int leftNum = nums[left++];
+                int f = freq.get(leftNum);
+                freq.put(leftNum, f - 1);
+                pairCount -= (f - 1);
             }
         }
-        return result;
+
+        return goodSubarrays;
     }
 }
---
+++ src/P2537/Solution1.java
@@ -0,0 +1,42 @@
+package P2537;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution1 {
+    public long countGood(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int b = 0;
+        int e = 0;
+        int n = nums.length;
+        int current = 0;
+        long result = 0;
+        count.put(nums[0], 1);
+        int lastB = -1;
+        int lastE = -1;
+        while (e < nums.length - 1) {
+            e++; 
+            current += count.getOrDefault(nums[e], 0);
+            System.out.println(current + " "+ b + " "+ e);
+            count.compute(nums[e], (key, v) -> v==null? 1: v +1);
+            if (current >= k) {
+                while (b < e && current - (count.get(nums[b]) - 1) >= k){
+                    current -= (count.get(nums[b]) - 1);
+                    count.compute(nums[b], (key, v) -> v-1);
+                    b++;
+                } 
+                if (lastB == -1) {
+                    result += (b + 1) * (n-e);
+                } else {
+                    result += (b - lastB) * (n - e );
+                }
+                lastB = b;
+                lastE = e; 
+                current -= (count.get(nums[b]) - 1);
+                count.compute(nums[b], (key, v) -> v - 1);
+                b++;
+            }
+        }
+        return result;
+    }
+}
